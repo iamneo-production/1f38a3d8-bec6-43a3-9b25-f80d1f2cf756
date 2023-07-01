ceafc0d81ac3e42c4ba94ad96bccd05401b22382
gbooo <gb00000009@gmail.com>
2023-07-01 08:08:14 +0000

    changes

--- reactapp/src/Settings/Content/pages/EditProfilePage.js
+++ reactapp/src/Settings/Content/pages/EditProfilePage.js
@@ -6,11 +6,13 @@ const EditProfilePage = () => {
       <div className='bg-white shadow-lg shadow-gray-300 rounded-lg h-[600px] w-9/12'>
         <div className='font-semibold text-3xl p-4 text-gray-800'>
           EditProfilePage
+
         </div>
 
         <div className='flex justify-center'>
           <div className='w-10/12 mt-3'>
             <div className='flex justify-between'>
+
               <div className='flex flex-col w-5/12'>
                 <label>First Name</label>
                 <input type='text' className='px-2 py-1 my-2 border rounded-lg' placeholder='Firstname' />
@@ -46,8 +48,10 @@ const EditProfilePage = () => {
         </div>
            
         <div className='mt-4 m-20 flex gap-3'>
+
           <button className='px-4 py-2 rounded-lg bg-blue-300 shadow-lg hover:shadow-xl hover:bg-blue-500'>Update </button>
           <button className='px-4 py-2 rounded-lg bg-gray-200 shadow-lg hover:shadow-xl hover:bg-gray-400 '> Cancel </button>
+
         </div>
 
       </div>
--- reactapp/src/Settings/Content/pages/SavePostPage.js
+++ reactapp/src/Settings/Content/pages/SavePostPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 const SavePostPage = () => {
   return (
+    
     <div>SavePostPage</div>
   )
 }
