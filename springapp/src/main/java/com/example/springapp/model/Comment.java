ef360ec19e91d2380e9584044d6eac5accb32022
gbooo <gb00000009@gmail.com>
2023-07-08 07:51:16 +0000

    changes

--- springapp/src/main/java/com/example/springapp/model/Comment.java
+++ springapp/src/main/java/com/example/springapp/model/Comment.java
@@ -1,56 +1,5 @@
 package com.example.springapp.model;
 
-<<<<<<< HEAD
-import javax.persistence.*;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
-public class Comment {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private String text;
-    private String createdDate;
-    
-    public Comment() {
-    }
-    
-    public Comment(int id, String text, String createdDate) {
-        this.id = id;
-        this.text = text;
-        this.createdDate = createdDate;
-    }
-    
-    // Getters and Setters
-    
-    public int getId() {
-        return id;
-    }
-    
-    public void setId(int id) {
-        this.id = id;
-    }
-    
-    public String getText() {
-        return text;
-    }
-    
-    public void setText(String text) {
-        this.text = text;
-    }
-    
-    public String getCreatedDate() {
-        return createdDate;
-    }
-    
-    public void setCreatedDate(String createdDate) {
-        this.createdDate = createdDate;
-    }
-}
-=======
 import java.time.LocalDate;
 
 import org.springframework.security.core.GrantedAuthority;
@@ -127,4 +76,3 @@ public class Comment {
         this.post = post;
     }
 }
->>>>>>> Social-Media-Portal-pratikmandge
