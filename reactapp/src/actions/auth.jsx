91d0078f4aaa6c0289b89f5f7f867aaa169ea978
gbooo <gb00000009@gmail.com>
2023-07-12 17:11:02 +0000

    changes

--- reactapp/src/actions/auth.jsx
+++ reactapp/src/actions/auth.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {LOGIN_FAIL,LOGIN_SUCCESS,SIGNUP_FAIL,SIGNUP_SUCCESS} from './types';
+import {LOGIN_FAIL,LOGIN_SUCCESS,SIGNUP_FAIL,SIGNUP_SUCCESS,USER_FETCH_SUCCESS,USER_FETCH_FAIL} from './types';
 
 
 
@@ -56,4 +56,47 @@ export const signup = (username,email, password,dateofbirth) => async dispatch =
             type: SIGNUP_FAIL
         })
     }
+};
+
+export const load_user = (username, password) => async dispatch => {
+    if (localStorage.getItem(token) ){
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',  
+                'Authorization':`Bearer ${localStorage.getItem(token)}`          
+            }
+        };
+        const body = JSON.stringify({ username, password });
+        try {
+            
+            const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/authenticate/`, body,config);
+            dispatch({
+                type: USER_FETCH_SUCCESS,
+                payload: res.data
+            });
+        } catch (err) {
+            if(err.response.status != 400){
+                dispatch({
+                    type: USER_FETCH_FAIL,
+                    payload: {message:"Something went wrong!!!  "}
+                })
+            }else{
+                dispatch({
+                    type: USER_FETCH_FAIL,
+                    payload: {message:"Invalid username or password"}
+                })
+            }
+
+        } 
+
+    }else{
+        dispatch({
+            type: USER_FETCH_FAIL,
+            payload: {message:"User is not authenticated"}
+        })
+
+    }
+
+        
 };
