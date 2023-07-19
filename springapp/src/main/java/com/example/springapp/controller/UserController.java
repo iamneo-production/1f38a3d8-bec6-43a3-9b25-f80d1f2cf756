2f3eaaacd8429c3a9a679e746775b0a921732dcd
ukeerthi18 <ukeerthipriya18@gmail.com>
2023-07-19 11:21:44 +0000

    commit

---
+++ springapp/src/main/java/com/example/springapp/controller/UserController.java
@@ -0,0 +1,67 @@
+package com.example.springapp.controller;
+
+import java.text.ParseException;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springapp.model.User;
+import com.example.springapp.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins="https://8081-cdeeceacaebfddcdafbacfedaceeaeaadbdbabf.project.examly.io/")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/")
+public class UserController{
+	
+	private final UserService userService;
+
+	@PostMapping("/api/users")
+		public ResponseEntity<User> createUser(@RequestBody User user) {
+    	User createdUser = userService.createUser(user);
+    	return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+	}
+
+
+	@GetMapping("/api/users/{userId}")
+    public ResponseEntity<User> getUserByUsername(@PathVariable("userId") String userId) {
+        User user = userService.getUserByUsername(userId);
+        return ResponseEntity.ok(user);
+    }
+
+	@GetMapping("/api/users")
+	public ResponseEntity<List<User>> getAllUserDetails() throws ParseException {
+		List<User> userList = userService.getAllUsers();
+		return ResponseEntity.ok(userList);
+	}
+
+	@PutMapping("/api/users/{userId}")
+    public ResponseEntity<String> updateUser(@PathVariable("userId") String userId, @RequestBody User updatedUser) {
+        userService.editUser(updatedUser, userId);
+        return ResponseEntity.ok("User updated successfully");
+    }
+
+	@DeleteMapping("/api/users/{userId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable("userId") String userId) {
+		if (userId != null){
+			userService.deleteUser(userId);
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		}
+		else {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+    }
+}
---
+++ springapp/src/main/java/com/example/springapp/service/PostService.java
@@ -0,0 +1,93 @@
+package com.example.springapp.service;
+
+import com.example.springapp.model.Post;
+import com.example.springapp.model.User;
+import com.example.springapp.service.UserService;
+import com.example.springapp.repository.PostRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.apache.commons.io.FileUtils;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+<<<<<<< HEAD
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+=======
+
+>>>>>>> Project-Workspace-u3m2a1
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
+    @Autowired
+    private final UserService userService;
+    private final PostRepository postRepository;
+
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+
+    public Post getPostById(int postId) {
+        return postRepository.findById(postId).orElseThrow(() -> new IllegalArgumentException("Post not found"));
+    }
+
+    public List<Post> getPostsByUser(String userId) {
+        User user = userService.getUserByUsername(userId);
+        return postRepository.findByUser(user);
+    }
+
+    public Post createPost(Post post,MultipartFile imageFile) {
+        User user = userService.getUserByUsername(post.getUser().getUsername());
+        post.setUser(user);
+        post.setTitle(post.getTitle());
+        post.setContent(post.getContent());
+        post.setCreatedAt(post.getCreatedAt());
+        post.setUpdatedAt(post.getUpdatedAt());
+
+        
+        if (imageFile != null && !imageFile.isEmpty()) {
+            try {
+                String fileName = StringUtils.cleanPath(imageFile.getOriginalFilename());
+                String fileExtension = StringUtils.getFilenameExtension(fileName);
+                String uniqueFileName = System.currentTimeMillis() + "." + fileExtension;
+
+                Path filePath = Path.of(uploadDir, uniqueFileName);
+                Files.copy(imageFile.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+                post.setImagePath(filePath.toString());
+            } catch (IOException e) {
+                // Handle the exception appropriately
+            }
+        }
+            
+        return postRepository.save(post);
+    }    
+
+    public Post updatePost(int postId, Post updatedPost) {
+        Post post = getPostById(postId);
+        post.setTitle(updatedPost.getTitle());
+        post.setContent(updatedPost.getContent());
+        return postRepository.save(post);
+    }
+
+    @Transactional
+    public void deletePost(int postId) {
+        Post post = getPostById(postId);
+        postRepository.delete(post);
+    }
+}
