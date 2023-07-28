c6923fb80cd6ac9b18058ed9db740a4491a45ce6
u3m2a1 <ukallepally6112001@gmail.com>
2023-07-28 17:56:14 +0000

    pulled changes from venkatesh

--- reactapp/src/components/Posts/CreatePost.jsx
+++ reactapp/src/components/Posts/CreatePost.jsx
@@ -1,48 +1,90 @@
-import React ,{useState}from 'react'
-import {BiEditAlt} from 'react-icons/bi'
-import {TbPhotoUp} from 'react-icons/tb'
-const CreatePost = ({postData,setPostData}) => {
+import React, { useState,useEffect } from 'react';
+import { BiEditAlt } from 'react-icons/bi';
+import { TbPhotoUp } from 'react-icons/tb';
+import axios from 'axios';
 
-  const [content, setcontent] = useState("");
-  const postDetails = {
-    id:1 , 
-    name:"bhavana",
-    active:"3", 
-    postImage:"https://upload.wikimedia.org/wikipedia/commons/2/24/Timisoara_-_Catholic_Dome_in_Union_Square.jpg",
-    profile:"https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
-    description:"I'm grateful to do this project",
-    likes:200,
-  }
+const CreatePost = ({ setPostData}) => {
+  const [content, setContent] = useState("");
+  const [file, setFile] = useState(null);
+  const [username,setUsername]=useState("");
 
-  const addPost = () =>{
+  useEffect(() => {
+      axios.get('https://8080-cffdafcacefddcdafbacfedaceeaeaadbdbabf.project.examly.io/api/posts')
+        .then(response => {
+          if (response.data.length > 0 && response.data[0].user) {
+            setUsername(response.data[0].user.username);
+          }
+        })
+        .catch(error => {
+          console.error('Error fetching user data:', error);
+        });
+  }, []);
 
-    postDetails["description"] = content;
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
 
-    setPostData(
-      [...postData,postDetails]
-    )
+  const addPost = () => {
+    const formData = new FormData();
+    if (file) {
+      formData.append("file", file);
+    }
 
-  }
+    const post = {
+      user: { username : username },
+      title: "New Post", 
+      content: content,
+    };
+    formData.append("post", JSON.stringify(post));
+    axios.post('https://8080-cffdafcacefddcdafbacfedaceeaeaadbdbabf.project.examly.io/api/posts', formData) 
+      .then(response => {
+        
+        setPostData(prevData => [...prevData, response.data]);
+        setContent("");
+        setFile(null);
+      })
+    .catch(error => {
+        console.error('Error creating post:', error);
+        if (error.response) {
+          console.error('Response data:', error.response.data);
+        }
+      });
+  };
 
   return (
     <div className='bg-white h-[280px] w-full my-5 rounded-xl'>
-        <div className='h-20 px-5 w-full flex pt-2 space-x-4 items-center' >
-            <div><BiEditAlt className='bg-gray-200 rounded-full px-2 text-blue-700 font-bold text-xl' size={40}/></div>
-            <p className='font-bold text-gray-500'>Create Post</p>
-        </div>
-        <div className='h-[150px] w-full bg-white p-3'>
-            <textarea onChange={(e) => setcontent(e.target.value)} className='w-full h-full rounded-lg border-2 p-2'placeholder="What's on your mind?"></textarea>
-        </div>
-        <div className='bg-white h- w-full px-4 flex justify-between space-x-2'>   
+      <div className='h-20 px-5 w-full flex pt-2 space-x-4 items-center'>
+        <div><BiEditAlt className='bg-gray-200 rounded-full px-2 text-blue-700 font-bold text-xl' size={40} /></div>
+        <p className='font-bold text-gray-500'>Create Post</p>
+      </div>
+      <div className='h-[150px] w-full bg-white p-3'>
+        <textarea
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          className='w-full h-full rounded-lg border-2 p-2'
+          placeholder="What's on your mind?"
+        ></textarea>
+      </div>
+      <div className='bg-white h- w-full px-4 flex justify-between space-x-2'>
         <div className='flex'>
-            <TbPhotoUp className='text-pink-500' size={30}/>
-            <input type="file" />
-            <p className='font-semibold text-gray-600'>Photo/Video</p>
+          <TbPhotoUp className='text-pink-500' size={30} />
+          <input
+            id="file-input"
+            type="file"
+            onChange={handleFileChange}
+          />
+          <p className='font-semibold text-gray-600'>Photo/Video</p>
         </div>
-            <button type='button' onClick={addPost} className='font-semibold p-2 bg-blue-700 rounded-lg hover:bg-blue-500 text-white '>submit</button>
-        </div>
+        <button
+          type='button'
+          onClick={addPost}
+          className='font-semibold p-2 bg-blue-700 rounded-lg hover:bg-blue-500 text-white '
+        >
+          submit
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CreatePost
+export default CreatePost;
--- reactapp/src/components/Posts/Posts.jsx
+++ reactapp/src/components/Posts/Posts.jsx
@@ -1,65 +1,75 @@
-import React,{useState} from 'react'
-import {AiOutlineHeart,AiFillHeart} from 'react-icons/ai'
-import {BiDotsHorizontalRounded} from 'react-icons/bi';
-import {VscBookmark} from 'react-icons/vsc';
-import {IoIosShareAlt} from 'react-icons/io';
+import React, { useState } from 'react';
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
+import { BiDotsHorizontalRounded } from 'react-icons/bi';
+import { VscBookmark } from 'react-icons/vsc';
 import CommentsSection from '../Comments/CommentSection';
+import Share from './Share';
+import axios from 'axios';
+import DropdownMenu from './Dropdownmenu';
 
-const Posts = ({post}) => {
+const Posts = ({ post }) => {
+  const [liked, setLiked] = useState(false);
 
-  const [liked, setliked] = useState(false);
-
-  const Like = () =>{
-
-    if(liked === true){
-      post.likes = post.likes-1;
-    }else{
-      post.likes = post.likes+1;
+  const handleLike = () => {
+    if (liked) {
+      axios.post(`https://8080-cffdafcacefddcdafbacfedaceeaeaadbdbabf.project.examly.io/api/posts/${post.id}/unlike`)
+        .then(response => {
+          setLiked(false);
+        })
+        .catch(error => {
+          console.error('Error unliking post:', error);
+        });
+    } else {
+      axios.post(`https://8080-cffdafcacefddcdafbacfedaceeaeaadbdbabf.project.examly.io/api/posts/${post.id}/like`)
+        .then(response => {
+          setLiked(true);
+          console.log(post.user.username);
+        })
+        .catch(error => {
+          console.error('Error liking post:', error);
+        });
     }
-    setliked(!liked);
-  }
-  
+  };
+  const likesCount = Array.isArray(post.likes) ? post.likes.length : 0;
+
   return (
-    <div className=' w-full bg-white my-2 rounded-lg'>
-        <div className='rounded-t-lg h-20 pt-6 px-5 w-full bg-white flex justify-between items-center'>
-          <div className='flex space-x-4 items-center'>
-            <img className="object-cover w-14 h-14 rounded-full" src={post.profile} alt="" />
-            <div >
-                <p className='text-lg font-semibold'>{post.name}</p>
-                <p className='text-md text-gray-500'>{post.active} min ago</p>
-            </div>
-          </div> 
-          <BiDotsHorizontalRounded size={30} />
+    <div className='w-full bg-white my-2 rounded-lg'>
+      <div className='rounded-t-lg h-20 pt-6 px-5 w-full bg-white flex justify-between items-center'>
+        <div className='flex space-x-4 items-center'>
+          <img className="object-cover w-14 h-14 rounded-full" src="https://i.pinimg.com/736x/3d/5f/e9/3d5fe98d6d997b8302a136625b88b532.jpg" alt="" />
+          <div>
+            <p className='text-lg font-semibold'>{post.user?.username}</p>
+            <p className='text-md text-gray-500'>{post.createdAt}</p>
+          </div>
         </div>
+        <DropdownMenu/>
+      </div>
 
-        <div className='h-[60px] bg-white w-full p-5'>
-            <p>{post.description} <span className='text-skyblue-700 font-semibold'>...see more </span></p>
+      <div className='h-[60px] bg-white w-full p-5'>
+        <p>{post.content} <span className='text-skyblue-700 font-semibold'>...see more </span></p>
+      </div>
+
+      {post.photoPath && (
+        <div className='h-[450px] w-full px-6 py-4'>
+          <img className="w-full h-full square-full rounded-xl" src={post.photoPath} alt="" />
         </div>
+      )}
 
-        <div className='h-[450px] w-full  px-6 py-4  '>
-                <img className=" w-full h-full square-full rounded-xl" src={post.postImage} alt="" />
+      <div className='flex justify-between mx-4 py-4'>
+        <div className="flex space-x-4">
+          {liked ? (
+            <AiFillHeart className='text-pink-800' size={30} onClick={handleLike} />
+          ) : (
+            <AiOutlineHeart size={30} onClick={handleLike} />
+          )}
+          <span className='px'>{likesCount} likes</span>
+          <CommentsSection />
+          <Share/>
         </div>
-        <div className=' flex justify-between mx-4 py-4'>
-            <div className="flex space-x-4">
-              {
-                liked ?
-                  <AiFillHeart className='text-pink-800' size={30} onClick={() => Like()} />
-                :
-
-                <AiOutlineHeart size={30} onClick={() => Like()} />
-              }  <span className='px'>{post.likes} likes</span>
-              
-              <CommentsSection/>
-              <IoIosShareAlt size={30}/>
-              
-            </div>
-            
-            <VscBookmark size={30}/>
-        </div>
-        {/* <CommentsSection/> */}
-
+        <VscBookmark size={30} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Posts
+export default Posts;
